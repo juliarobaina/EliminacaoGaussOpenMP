89523c0d5a9f8db18b27f8e83e5f16a8101770ed
Julia Robaina <47131530+juliarobaina@users.noreply.github.com>
2023-07-28 11:08:29 -0300

    Update mainMetodosDiretos.c

--- mainMetodosDiretos.c
+++ mainMetodosDiretos.c
@@ -7,7 +7,7 @@ int main(int argc, char *argv[])
 
     int ordem = atoi(argv[1]);
 
-    FILE *A = fopen("E.txt", "r"), *B = fopen("F.txt", "r"); // carrega arquivos
+    FILE *A = fopen("A.txt", "r"), *B = fopen("B.txt", "r"); // carrega arquivos
     double *vetorSolucao = alocarVetor(ordem);
 
     eliminacaoGauss(A, B, ordem);
